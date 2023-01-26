bbf9d8e0becadab798b1a203e7b7832a97d1f450
Memo <mehmetxeris@gmail.com>
2023-01-26 22:54:29 +0100

    Initial Commit

---
+++ domates/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  domates
+//
+//  Created by Mehmet Sinan Eris on 26.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ domates/domatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  domatesApp.swift
+//  domates
+//
+//  Created by Mehmet Sinan Eris on 26.01.23.
+//
+
+import SwiftUI
+
+@main
+struct domatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
