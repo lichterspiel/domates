2c8f3f2e9d4f6077da92d8db3722c97367ce3380
Memo <mehmetxeris@gmail.com>
2023-02-11 17:09:17 +0100

    add timer

---
+++ domates/DomatesTimer.swift
@@ -0,0 +1,107 @@
+//
+//  Timer.swift
+//  domates
+//
+//  Created by Mehmet Sinan Eris on 08.02.23.
+//
+
+import SwiftUI
+
+class DomatesTimer: ObservableObject {
+    private static var domatesTimer: DomatesTimer?
+    
+    private var studyIntervall: Int
+    private var breakIntervall: Int
+    private var studyRounds: Int
+    private var timer: Timer?
+    
+    
+    @Published public var elapsedSeconds: Int = 0
+    @Published public var elapsedRounds: Int = 0
+    @Published public var isRunning: Bool = false
+    
+    init(studyIntervall: Int, breakIntervall: Int, studyRounds: Int) {
+        self.studyIntervall = studyIntervall
+        self.breakIntervall = breakIntervall
+        self.studyRounds = studyRounds
+    }
+    
+    
+    public func startTimer(){
+        if self.elapsedRounds != self.studyRounds * 2{
+            if self.elapsedRounds % 2 == 0{
+                self.doTime(isStudy: true)
+            }
+            else {
+                self.doTime(isStudy: false)
+            }
+            
+            isRunning = true
+        }
+        else {
+            self.stopTimer()
+            self.elapsedRounds = 0
+            isRunning = false
+        }
+    }
+    
+    private func doTime(isStudy: Bool){
+        let seconds = isStudy ? studyIntervall * 60  : breakIntervall * 60
+            
+        let timeString = String(
+           format: "%.2i:%.2i",
+           (seconds - self.elapsedSeconds) / 60,
+           (seconds - self.elapsedSeconds) % 60
+        )
+        AppDelegate.shared.statusBar?.setTitle(timeString)
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in
+            self.elapsedSeconds += 1
+            
+            let timeString = String(
+               format: "%.2i:%.2i",
+               (seconds - self.elapsedSeconds) / 60,
+               (seconds - self.elapsedSeconds) % 60
+            )
+            AppDelegate.shared.statusBar?.setTitle(timeString)
+            
+            if self.elapsedSeconds == seconds{
+                
+                self.elapsedRounds += 1
+            
+                self.stopTimer()
+                self.startTimer()
+            }
+        }
+    }
+    
+    public func stopTimer(){
+        self.timer?.invalidate()
+        self.timer = nil
+        self.elapsedSeconds = 0
+        
+        AppDelegate.shared.statusBar?.setTitle("")
+    }
+    
+    public func setStudyIntervall(intervall: Int){
+        self.studyIntervall = intervall
+    }
+    
+    public func setBreakIntervall(intervall: Int){
+        self.breakIntervall = intervall
+    }
+    
+    public func setRounds(rounds: Int){
+        self.studyRounds = rounds
+    }
+    
+    public static func getTimer() -> DomatesTimer{
+        if Self.domatesTimer == nil {
+            Self.domatesTimer = DomatesTimer(studyIntervall: 25, breakIntervall: 5, studyRounds: 4)
+        }
+        
+        return Self.domatesTimer!
+    }
+    
+    
+}
